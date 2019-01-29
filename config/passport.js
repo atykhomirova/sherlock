7a77f54f5ce2cd320be3a41e8ae7f50372ba6520
Anastasiia <kucherenko.a.a@gmail.com>
2019-01-29 08:56:29 +0200

    deleted constant value

--- config/passport.js
+++ config/passport.js
@@ -8,7 +8,7 @@ passport.use(new LocalStrategy({
   usernameField: 'userProfile[email]',
   passwordField: 'userProfile[password]',
 }, (email, password, done) => {
-  UserProfiles.findOne({ emails: {$elemMatch: { email: 'testemail2@gmail.com'}}})
+  UserProfiles.findOne({ emails: {$elemMatch: { email: email}}})
     .then((userProfile) => {
       if(!userProfile || !userProfile.validatePassword(password)) {
         return done(null, false, { errors: { 'email or password': 'is invalid' } });
